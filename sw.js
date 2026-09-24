3932e6a3d32db32ea499c27e2f193c4ca15065d4
classamkun-cyber <classa.m.kun@gmail.com>
2026-09-24 19:13:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nespaPT-v21.1';
+const CACHE = 'nespaPT-v21.2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.png', './icon-192.png', './icon-180.png'];
 
 self.addEventListener('install', (e) => {
